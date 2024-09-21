a87e00ec273d99e8b82a7d166bed9e1cee13ea55
Jun Wei <junwei.wong@yahoo.com>
2024-09-21 16:59:17 +0800

    Added the last files

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "3179 Week 9 HW.json";
+
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
